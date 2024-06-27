f2d50434d4a56d0a6b84eff461f5964a45dd1337
Ian Kimani <ian.kiarie2002@gmail.com>
2024-06-27 15:14:57 +0300

    complete

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -3,9 +3,17 @@ import { PrismaClient } from '@prisma/client'
 const router = Router();
 const prisma = new PrismaClient();
 
-router.get("/", (req, res) => {
-    res.send("getting all the products");
+router.get("/", async (req, res) => {
+    try{
+        const products = await prisma.products.findMany()
+        res.status(200).json(products)
+    }catch(e){
+        res.status(500).json({success: false, message: e.message})
+
+    }
+    // res.send("getting all the products");
 })
+
 
 router.post("/", async (req, res) => {
     try{
@@ -19,31 +27,56 @@ router.post("/", async (req, res) => {
                 onOffer: onOffer
                 },
                 select: {
+                    id: true,
                     productThumbnail: true,
                     productTitle: true,
                     productDescription: true,
                     productCost: true
                 }
         })
-        res.status(201).json(products);
-
+        res.status(201).json( {success: true, data: products});
     }
     catch(e){
-        res.status(500).json({ success: false, message: e.message});
+        res.status(500).json({ success: false, message: "FAILURE!!!!"});
+    }
+})
+
+router.get("/:id", async (req, res) => {
+    try{
+        const products = await prisma.products.findFirst({
+            where: {id: req.params.id}
+        })
+            res.status(200).json(products)
+    }catch(e){
+        res.status(500).json({success: false, message: e.message})
     }
     // res.send("getting a single product");
 })
 
-router.get("/:id", (req, res) => {
-    res.send("getting a single product");
+router.patch("/:id", async (req, res) => {
+    try{
+        const updatedproducts = await prisma.products.update({
+            where: {id: req.params.id},
+            data:{productDescription: "it is cheaper and sweeter"}
+    })
+    res.status(201).json(updatedproducts) 
+        //comfirm when there is internet connection
+    }catch(e){
+        res.status(500).json({success: false, message: e.message})
+    }
+    // res.send("updating a product");
 })
 
-router.patch("/:id", (req, res) => {
-    res.send("updating a product");
-})
-
-router.delete("/:id", (req, res) => {
-    res.send("deleting a product");
+router.delete("/:id", async (req, res) => {
+    try{
+        const products = await prisma.products.delete({
+            where: {id: req.params.id}
+        })
+        res.status(200).json(products)
+    }catch(error){
+        res.status(500).json({success: false, message: e.message})
+    }
+    // res.send("deleting a product");
 })
 
 export default router;
